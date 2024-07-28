13815a0741b25771f358cd382c542575f552c88a
shoebilyas123 <shoebilyas123@gmail.com>
2024-07-28 13:31:39 +0530

    feat: parser error handling

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -18,6 +18,7 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
@@ -43,6 +44,18 @@ func TestLetStatements(t *testing.T) {
 		}
 	}
 
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
