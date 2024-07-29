44bfd44245a226bdc70f981f1f9b79eace7dbfad
shoebilyas123 <shoebilyas123@gmail.com>
2024-07-29 12:09:00 +0530

    feat: print statements in parser String() method

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -5,7 +5,29 @@ import (
 
 	"github.com/shoebilyas123/monkeylang/monkey/ast"
 	"github.com/shoebilyas123/monkeylang/monkey/lexer"
+	"github.com/shoebilyas123/monkeylang/monkey/token"
 )
+
+func TestString(t *testing.T) {
+	program := &ast.Program{
+		Statements: []ast.Statement{
+			&ast.LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar"},
+				Value: &ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
 
 func TestLetStatements(t *testing.T) {
 	input := `
